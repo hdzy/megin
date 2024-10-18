332805aece723db27dd79db4ca150db3d200fe92
Smirnov Oleg <smirnov.oleg.1231@gmail.com>
2024-10-18 16:02:33 +0300

    mob

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@ const reviews = new Swiper('.reviews-swiper', {
 });
 
 const productMob = new Swiper('.product-swiper', {
-    slidesPerView: 1,
+    slidesPerView: 'auto',
     spaceBetween: 20,
     loop: true,
     grabCursor: true,
@@ -21,7 +21,7 @@ const productMob = new Swiper('.product-swiper', {
 });
 
 const advantagesMob = new Swiper('.cards-swiper', {
-    slidesPerView: 1,
+    slidesPerView: 'auto',
     spaceBetween: 20,
     loop: true,
     grabCursor: true,
@@ -42,6 +42,27 @@ const gallery = new Swiper('.swiper-gallery', {
     },
 })
 
+const galleryMob = new Swiper('.swiper-gallery-mob', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: '.gallery-mob-pagination',
+        clickable: true,
+    },
+})
+
+const reviewsMob = new Swiper('.reviews-mob-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: '.reviews-mob-pagination',
+        clickable: true,
+    },
+})
 document.querySelectorAll('.qa-section .question').forEach((ev) => {
    ev.addEventListener('click', (e) => {
        let target = e.target;
@@ -60,4 +81,35 @@ document.querySelectorAll('.qa-section .question').forEach((ev) => {
            target.classList.add('active');
        }
    })
+})
+
+
+
+document.querySelector('.header--cont .nav-link.green').addEventListener('click', () => {
+    document.body.style = 'overflow: hidden';
+    document.querySelector('#f-pop').classList.add('active');
+})
+
+document.querySelector('#f-pop').addEventListener('click', (e) => {
+    if (e.target.classList.contains('reply-pop')) {
+        document.body.style = '';
+        document.querySelector('#f-pop').classList.remove('active');
+    }
+})
+
+document.querySelector('#f-pop .default-btn').addEventListener('click', () => {
+    document.querySelector('#f-pop').classList.remove('active');
+    document.querySelector('#s-pop').classList.add('active');
+})
+
+document.querySelector('#s-pop').addEventListener('click', (e) => {
+    if (e.target.classList.contains('reply-pop')) {
+        document.body.style = '';
+        document.querySelector('#s-pop').classList.remove('active');
+    }
+})
+
+document.querySelector('#s-pop .default-btn').addEventListener('click', (e) => {
+        document.body.style = '';
+        document.querySelector('#s-pop').classList.remove('active');
 })
